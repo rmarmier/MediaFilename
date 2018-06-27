4c7678ae03b59e065364631ad646401e6f6d1a08
raphaelm <raphael@marmier.net>
2018-06-27 15:29:50 +0200

    Properly support files with leading dot or with extension missing.

--- src/main/java/net/marmier/mediafilename/filename/FilenameHelper.java
+++ src/main/java/net/marmier/mediafilename/filename/FilenameHelper.java
@@ -1,5 +1,7 @@
 package net.marmier.mediafilename.filename;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,21 +10,37 @@ import java.util.regex.Pattern;
  * Added by raphael on 06.10.16.
  */
 public class FilenameHelper {
-        private static Pattern STRIP_EXTENSION_PATTERN = Pattern.compile("^([/]?([^/\\n]+/)*([^./\\n]+))(\\.([a-zA-Z]+))?$");
+    private static Pattern OLD_STRIP_EXTENSION_PATTERN =
+        Pattern.compile("^([/]?([^/\\n]+/)*([^./\\n]+))(\\.([a-zA-Z]+))?$");
+    // ^((\B\.)?(([^./\n]+\.)+|[^./\n]+))([^./\n]+)?$
+    private static Pattern STRIP_EXTENSION_PATTERN =
+        Pattern.compile("^((\\B\\.)?(([^./\\n]+\\.)+|[^./\\n]+))([^./\\n]+)?$");
 
-    public static String stripExtension(String filename) {
-        Objects.requireNonNull(filename);
+    public static String stripExtension(String path) {
+        Objects.requireNonNull(path);
+        Path p = new File(path).toPath();
+        String filename = p.getFileName().toString();
+        Path parent = p.getParent();
 
         Matcher matcher = STRIP_EXTENSION_PATTERN.matcher(filename);
         if (matcher.matches()) {
-            return matcher.group(1);
+            String stripped = matcher.group(1);
+            if (stripped.endsWith(".") && stripped.length() > 1 && matcher.group(4) != null) {
+                stripped = stripped.substring(0, stripped.length() - 1);
+            }
+            if (parent != null) {
+                return parent.resolve(stripped).toString();
+            } else {
+                return stripped;
+            }
         } else {
             return null;
         }
     }
 
-    public static String getExtension(String filename) {
-        Objects.requireNonNull(filename);
+    public static String getExtension(String path) {
+        Objects.requireNonNull(path);
+        String filename = new File(path).toPath().getFileName().toString();
 
         Matcher matcher = STRIP_EXTENSION_PATTERN.matcher(filename);
         if (matcher.matches()) {
--- src/test/java/net/marmier/mediafilename/filename/FilenameHelperTest.java
+++ src/test/java/net/marmier/mediafilename/filename/FilenameHelperTest.java
@@ -1,8 +1,12 @@
 package net.marmier.mediafilename.filename;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * Added by raphael on 06.10.16.
@@ -13,6 +17,7 @@ public class FilenameHelperTest {
     public void testStripExtension() throws Exception {
         assertEquals("myFile", FilenameHelper.stripExtension("myFile.png"));
     }
+
     @Test
     public void testStripExtensionWithPath() throws Exception {
         assertEquals("mypath/myFile", FilenameHelper.stripExtension("mypath/myFile.png"));
@@ -20,13 +25,66 @@ public class FilenameHelperTest {
     }
 
     @Test
+    public void testStripExtensionDotFile() throws Exception {
+        assertEquals(".myDotFile", FilenameHelper.stripExtension(".myDotFile.png"));
+        assertEquals("mypath/.myDotFile", FilenameHelper.stripExtension("mypath/.myDotFile.png"));
+        assertEquals("/mypath/mypath/.myDotFile", FilenameHelper.stripExtension("/mypath/mypath/.myDotFile.png"));
+    }
+
+    @Test
+    public void testStripExtensionDotInTheMiddle() {
+        assertEquals("myFile.blahblah.myDotFile", FilenameHelper.stripExtension("myFile.blahblah.myDotFile.png"));
+        assertEquals(".myFile.blahblah.myDotFile", FilenameHelper.stripExtension(".myFile.blahblah.myDotFile.png"));
+        assertEquals("mypath/.myFile.myDotFile", FilenameHelper.stripExtension("mypath/.myFile.myDotFile.png"));
+    }
+
+    @Test
+    public void unsupportedCornerCasesToFixIfPossible() {
+        // Some unsupported corner cases that shoul not happen in real life
+        assertNull(FilenameHelper.stripExtension("mypath/..myDotFile.png"));
+        assertNull(FilenameHelper.stripExtension("mypath/..myDo..tFile.png"));
+        assertNull(FilenameHelper.stripExtension("mypath/myDotFile.png.."));
+        assertNull(FilenameHelper.stripExtension("mypath/..png"));
+    }
+
+    @Test
+    public void testStripExtensionDotFileWithoutExtention() throws Exception {
+        assertEquals(".myDotFileWithoutExtention", FilenameHelper.stripExtension(".myDotFileWithoutExtention"));
+        assertEquals("mypath/mypath/.myDotFileWithoutExtention", FilenameHelper.stripExtension("mypath/mypath/.myDotFileWithoutExtention"));
+        assertEquals("/mypath/mypath/.myDotFileWithoutExtention", FilenameHelper.stripExtension("/mypath/mypath/.myDotFileWithoutExtention"));
+    }
+
+    @Test
+    public void testStripExtensionFileWithoutExtention() throws Exception {
+        assertEquals("myFileWithoutExtention", FilenameHelper.stripExtension("myFileWithoutExtention"));
+        assertEquals("mypath/mypath/myFileWithoutExtention", FilenameHelper.stripExtension("mypath/mypath/myFileWithoutExtention"));
+        assertEquals("/mypath/mypath/myFileWithoutExtention", FilenameHelper.stripExtension("/mypath/mypath/myFileWithoutExtention"));
+    }
+
+    @Test
     public void testGetExtension() throws Exception {
         assertEquals("png", FilenameHelper.getExtension("myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension(".myFile.png"));
+        assertNull(FilenameHelper.getExtension("myFile"));
+        assertNull(FilenameHelper.getExtension(".myFile"));
+    }
+
+    @Test
+    public void testGetExtensionDotInTheMiddle() {
+        assertEquals("png", FilenameHelper.getExtension("blahblah.myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension(".blahblah.myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension("/mypath/mypath/blahblah.myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension("/mypath/mypath/.blahblah.myFile.png"));
     }
 
     @Test
     public void testGetExtensionWithPath() throws Exception {
         assertEquals("png", FilenameHelper.getExtension("mypath/myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension("/mypath/myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension("/mypath/mypath/myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension("/mypath/mypath/mypath/myFile.png"));
         assertEquals("png", FilenameHelper.getExtension("mypa.th/myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension("mypath/mypa.th/myFile.png"));
+        assertEquals("png", FilenameHelper.getExtension("/mypath/mypa.th/myFile.png"));
     }
 }
